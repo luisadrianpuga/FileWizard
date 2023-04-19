58c03d003413d0eade2ed489bb426ed0393f8b2f
Luis Puga <duhluis.p@gmail.com>
2023-04-19 14:24:47 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FileWizard
+
+The tool will organize files in the directory and its subdirectories based on their file type and move them to the appropriate folders.
+
+## Customization
+
+The `file_categories` dictionary can be customized to include additional categories and file extensions as needed. Note that the script is designed to organize files by extension, so it may not work well for files with complex or non-standard file names.
+
+## Contributing
+
+If you find a bug or have a feature request, please open an issue or submit a pull request. Contributions are welcome!
+
+## License
+
+This tool is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+Feel free to modify this README file as needed for your project. You may want to include additional sections such as a description of the `file_categories` dictionary, examples of file names and extensions that are not recognized by the tool, or instructions for running the tool on different operating systems.
---
+++ file_wizard.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import argparse
+
+def file_wizard(directory):
+    
+    file_categories = {
+        'documents': ['.pdf', '.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.rtf', '.odt'],
+        'images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.psd', '.ai', '.eps', '.PNG', '.webp'],
+        'videos': ['.mp4', '.mkv', '.flv', '.avi', '.mov', '.wmv', '.mpeg', '.mpg', '.webm', '.MOV'],
+        'audio': ['.mp3', '.wav', '.m4a', '.flac', '.ogg', '.aac', '.wma'],
+        'archives': ['.zip', '.rar', '.tar', '.gz', '.7z', '.iso', '.dmg', '.apk'],
+        'programs': ['.exe', '.msi', '.dmg', '.pkg', '.deb', '.rpm'],
+        'code': ['.py', '.cpp', '.h', '.html', '.css', '.js', '.java', '.rb', '.pl', '.swift', '.json', '.key', '.pem'],
+        'fonts': ['.ttf', '.otf', '.woff', '.woff2'],
+        'other': ['.localized']
+    }
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_ext = os.path.splitext(file)[1]
+
+            for category, extensions in file_categories.items():
+                if file_ext in extensions:
+                    category_path = os.path.join(directory, category)
+
+                    if not os.path.exists(category_path):
+                        os.makedirs(category_path)
+
+                    shutil.move(file_path, os.path.join(category_path, file))
+                    print(f"Moved {file} to {category} folder")
+                    break
+
+def main():
+    parser = argparse.ArgumentParser(description='Organize files based on their file type.')
+    parser.add_argument('directory', type=str, help='Directory to organize')
+    args = parser.parse_args()
+
+    file_wizard(args.directory)
+
+if __name__ == '__main__':
+    main()
